06b350f9a8313faa39d85bf8d2b4de5fab4c8291
Оля Урбан < @DESKTOP-FI63LIH>
2017-01-10 16:02:58 +0300

    Добавьте файлы проекта.

---
+++ Entrants/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Entrants.Startup))]
+namespace Entrants
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
